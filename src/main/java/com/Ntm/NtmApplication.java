614d833271808ea4483578a373b42c83cf83b2a6
DiegoT <d.torres19@ufromail.cl>
2026-05-10 15:57:37 -0400

    Rama base del proyecto Ntm.

---
+++ src/main/java/com/Ntm/NtmApplication.java
@@ -0,0 +1,13 @@
+package com.Ntm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Ntm/NtmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ntm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
