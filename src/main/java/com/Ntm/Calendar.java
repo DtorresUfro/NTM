069f75c5093a3d1447a58bd1f8e714eb67a5274c
DiegoT <d.torres19@ufromail.cl>
2026-05-11 11:56:07 -0400

    feature/Clases: Estructura de clases agregada 'Calendar' y 'Room'

--- src/main/java/com/Ntm/Calendar.java
+++ src/main/java/com/Ntm/Calendar.java
@@ -1,4 +1,21 @@
 package com.Ntm;
 
+import java.util.ArrayList;
+
 public class Calendar {
+    private ArrayList<Task> tareas;
+    private ArrayList<Note> notas;
+
+    public void addTask(Task tarea){
+
+    }
+    public void addNote(Note nota){
+
+    }
+    public ArrayList<Task> getTasks(){
+        return null;
+    }
+    public ArrayList<Note> getNotes(){
+        return null;
+    }
 }
--- src/main/java/com/Ntm/Room.java
+++ src/main/java/com/Ntm/Room.java
@@ -1,4 +1,22 @@
 package com.Ntm;
 
+import java.util.Date;
+
 public class Room {
+    private String name;
+    private String masterKey;
+    private String id;
+    private Date createdAt;
+    private Date lastActivity;
+    private Calendar calendario;
+
+    public String generateJoinId(){
+        return "";
+    }
+    public String generateMasterKey(){
+        return "";
+    }
+    public void transferAdmin(){
+
+    }
 }
