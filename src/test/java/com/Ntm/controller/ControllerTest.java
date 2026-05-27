56ce27caa576d1d493e3981183b38ee47d009876
Barbara <b.sanhueza07@ufromail.cl>
2026-05-27 14:25:40 -0400

    Corregion de nombre de clase ControllerTest

--- src/test/java/com/Ntm/controller/ControllerTest.java
+++ src/test/java/com/Ntm/controller/ControllerTest.java
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(RoomController.class)
-class RoomControllerTest {
+class ControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
