0ec7f4a6cda8398c2429cc4abb02137b3ee99759
DiegoT <d.torres19@ufromail.cl>
2026-06-15 03:24:51 -0400

    Added: Test para DTOs del proyecto.

---
+++ src/test/java/com/Ntm/dto/AdminAccessResponseTest.java
@@ -0,0 +1,26 @@
+package com.Ntm.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AdminAccessResponseTest {
+    @Test
+    void shouldSetAndGetValues() {
+        AdminAccessResponse response = new AdminAccessResponse();
+
+        response.setRoomId("ROOM-123");
+        response.setAdminName("Dyssio");
+
+        assertEquals("ROOM-123", response.getRoomId());
+        assertEquals("Dyssio", response.getAdminName());
+    }
+
+    @Test
+    void shouldCreateUsingConstructor() {
+        AdminAccessResponse response = new AdminAccessResponse("ROOM-123", "Dyssio");
+
+        assertEquals("ROOM-123", response.getRoomId());
+        assertEquals("Dyssio", response.getAdminName());
+    }
+}
