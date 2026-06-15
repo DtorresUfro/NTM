bbaddb797d4a4f7e11f579c331dbf166e2c141d5
DiegoT <d.torres19@ufromail.cl>
2026-06-15 04:52:46 -0400

    Added: Test de cobertura para Calendar y Note.

---
+++ src/test/java/com/Ntm/entity/CalendarTest.java
@@ -0,0 +1,15 @@
+package com.Ntm.entity;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalendarTest {
+    @Test
+    void shouldManageIdCorrectly() {
+        Calendar calendar = new Calendar();
+
+        calendar.setId(100L);
+
+        assertEquals(100L, calendar.getId());
+    }
+}
