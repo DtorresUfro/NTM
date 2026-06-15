05480e64f8c9d05a85a196716e8d47ceab415b2d
DiegoT <d.torres19@ufromail.cl>
2026-06-15 03:55:04 -0400

    Added: Test para ViewController.

--- src/test/java/com/Ntm/controller/ViewControllerTest.java
+++ src/test/java/com/Ntm/controller/ViewControllerTest.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import java.util.List;
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
